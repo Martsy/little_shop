e524eb7660485ccfcb2d4e5ea2a01c9b9a3f9cfe
Martha Troubh <indexer@icloud.com>
2019-07-11 13:10:33 -0600

    Refactors

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
   has_many :order_items
   has_many :items, through: :order_items
+  
+  validates_presence_of :name, :address, :city, :state, :zip
 end
