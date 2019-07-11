0ab8a00fe7e62fa0bbdd21b0756518d3d88d1a3d
Martha Troubh <indexer@icloud.com>
2019-07-11 13:05:14 -0600

    Refactor and coverage

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Cart do
-  subject { Cart.new({'1' => 2, '2' => 3}) }
+  subject { Cart.new('1' => 2, '2' => 3) }
 
-  describe "#total_count" do
-    it "calculates the total number of items it holds" do
+  describe '#total_count' do
+    it 'calculates the total number of items it holds' do
       expect(subject.total_count).to eq(5)
     end
   end
 
-  describe "#add_item" do
-    it "adds a item to its contents" do
-      cart = Cart.new({
-        '1' => 2,  # two copies of item 1
-        '2' => 3   # three copies of item 2
-      })
+  describe '#add_item' do
+    it 'adds a item to its contents' do
+      cart = Cart.new(
+        '1' => 2, # two copies of item 1
+        '2' => 3 # three copies of item 2
+      )
       subject.add_item(1)
       subject.add_item(2)
 
-      expect(subject.contents).to eq({'1' => 3, '2' => 4})
+      expect(subject.contents).to eq('1' => 3, '2' => 4)
     end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Item do
   describe 'Relationships' do
-    it {should belong_to :merchant}
+    it { should belong_to :merchant }
+    it { should have_many :order_items }
+    it { should have_many(:orders).through(:order_items) }
+    it { should have_many :reviews }
+
+    describe 'Validations' do
+      it { should validate_presence_of :name }
+      it { should validate_presence_of :description }
+      it { should validate_presence_of :price }
+      it { should validate_presence_of :image }
+      it { should validate_presence_of :inventory }
+    end
+
+    describe '#ordered' do
+      it 'returns boolean based on item order' do
+        megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80_218)
+        brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80_218)
+
+        hippo = brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3)
+        giant = megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3)
+        order1 =Order.create!(name: 'Jack', address: '123 Smith Road', city: 'Denver', state: 'CO', zip: 80202)
+        OrderItem.create!(order: order1, item: hippo, quantity: 1, price: 50)
+
+        expect(giant.ordered?).to eq(false)
+        expect(hippo.ordered?).to eq(true)
+      end
+    end
   end
 end
