cf6297089beb9df8cd2c8663b3fd79f238623c88
Martha Troubh <indexer@icloud.com>
2019-07-13 18:33:21 -0600

    Merchant model stat methods added
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,22 @@
+# frozen_string_literal: true
+
 class Merchant < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :items, dependent: :destroy
 
   def ordered?
     items.joins(:order_items).count != 0
+  end
+
+  def merch_sold
+    items.count
+  end
+
+  def average_price
+    items.average(:price).to_f
+  end
+
+  def cities
+    items.joins(:orders).distinct.pluck(:city)
   end
 end
